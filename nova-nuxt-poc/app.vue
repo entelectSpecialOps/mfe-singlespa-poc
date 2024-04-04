76fda398404f0d47421e488bd4a662803bcb360c
Michael Gomes <michael.gomes12@gmail.com>
2024-04-04 12:15:12 +0200

    Amend nuxt application to make use of Angular Components

--- nova-nuxt-poc/app.vue
+++ nova-nuxt-poc/app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <clients-provider></clients-provider>
+    <country-provider></country-provider>
+    <country-list-provider></country-list-provider>
   </div>
 </template>
--- nova-nuxt-poc/nuxt.config.ts
+++ nova-nuxt-poc/nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+      head: {
+         script: [
+             { type:"text/javascript",src:"http://localhost:8080/main.js"}
+         ]
+     }
+    }
 })
